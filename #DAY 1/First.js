b2352e9887dfdce5c03b790c06b0c2760b24bf07
TUSHAR PASRIJA <66158870+id-007@users.noreply.github.com>
2020-07-15 07:11:40 +0530

    Add files via upload

---
+++ #DAY 1/First.js
@@ -0,0 +1,20 @@
+ alert("hey this is a js based page");
+console.log("i am on line 2 :)");
+console.info("i am console info");
+console.error("there is a error !!! please check your code");
+console.warn("lets upgrade");
+console.count(document.body);
+console.table(["mango","orange","banana"]);
+
+
+// console.time('total time');
+var a = {name:"tushar", course:"Javascript", platform :"Youtube"}
+
+var b = {name:"Raghav" , course:"Python" , platform :"dailymotion"}
+
+var c = {name:"vishal" , course:"C#" , platform:"Facebook"}
+
+console.log({a ,b ,c});
+// console.timeEnd('end of time');
+
+
---
+++ #DAY 1/second.js
@@ -0,0 +1,26 @@
+// var statement
+var a = 20;
+var a = 30;
+console.log(a);
+// conclution  : //var can be redefined and updated;
+
+
+
+// let statement
+{
+    let a = 50;
+    a = 90;
+     //let a = 80;
+    console.log(a);
+}
+// conclusion : // let keyword can't be redefined but can be updated
+
+
+
+//const statement
+const fruits = ["apple","pineapple","strawberry"]
+console.log(fruits);
+//  fruits = ["blackberry","cherry"];
+// console.log(fruits);
+
+// conclusion : // const keyword can't be redefined or updated
